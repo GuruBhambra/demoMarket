a4e23fe886d235e8f77121ac189e51dc5b1b353d
GuruBhambra <gurupreet108@gmail.com>
2023-01-31 11:56:32 +0530

    New Search File Added

---
+++ src/main/java/RestAPI/RestAssuredDemo/searchTextTest.java
@@ -0,0 +1,11 @@
+package RestAPI.RestAssuredDemo;
+
+public class searchTextTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello u have successfully searched me");
+
+	}
+
+}
